00958b916fd2b80de761472cc4fde0b1241fbb1d
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-23 18:51:35 +0100

    `install` `prepare`

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,58 @@
-module.exports = {
-  compact: true,
-  comments: false,
-  presets: [
-    [
-      '@babel/env', {
-        targets: {
-          node: 'current',
-          browsers: [
-            'last 2 versions'
-          ]
-        },
-        useBuiltIns: 'usage',
-        corejs: 3
-      }
-    ]
-  ],
-  plugins: [
-    '@babel/proposal-export-default-from',
-    '@babel/transform-runtime',
-    [
-      'module-resolver', {
-        root: ['./src'],
-        cwd: 'babelrc',
-        alias: {
-          zashiki: './src'
-        }
-      }
-    ]
+const debug = require('debug')
+
+const log = debug('zashiki')
+
+const {
+  env: {
+    DEBUG = 'zashiki',
+    NODE_ENV = 'development'
+  }
+} = process
+
+debug.enable(DEBUG)
+
+const presets = [
+  [
+    '@babel/env', {
+      targets: {
+        node: 'current',
+        browsers: [
+          'last 2 versions'
+        ]
+      },
+      useBuiltIns: 'usage',
+      corejs: 3
+    }
   ]
+]
+
+const plugins = [
+  '@babel/proposal-export-default-from',
+  '@babel/transform-runtime',
+  [
+    'module-resolver', {
+      root: ['./src'],
+      cwd: 'babelrc',
+      alias: {
+        zashiki: './src'
+      }
+    }
+  ]
+]
+
+function using () {
+  log({ NODE_ENV })
+
+  return NODE_ENV === 'production'
+}
+
+module.exports = (api) => {
+  if (api) api.cache.using(using)
+
+  return {
+    compact: true,
+    comments: false,
+    presets,
+    plugins
+  }
 }
