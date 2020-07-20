43744338174b55707f46b1d1b941c8a22511b3aa
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-07-20 10:35:57 +0100

    `ZID`

--- src/common/pre/jid/index.js
+++ src/common/pre/jid/index.js
@@ -3,14 +3,14 @@ import Boom from '@hapi/boom'
 import zashiki from 'zashiki/config'
 
 import {
-  createJID
+  createZID
 } from '@modernpoacher/zashiki-transport'
 
 import {
   BAD_IMPLEMENTATION
 } from 'zashiki/common/exception'
 
-const JID = zashiki.get('zashiki:jid')
+const ZID = zashiki.get('zashiki:jid')
 const BEARERTOKEN = /[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i
 const getBearerToken = (value = '') => (value.match(BEARERTOKEN) || []).shift()
 
@@ -22,7 +22,7 @@ export default {
     } = state
 
     try {
-      h.state('jid', jid || (jid = await createJID()), JID)
+      h.state('jid', jid || (jid = await createZID()), ZID)
 
       return jid
     } catch (e) {
