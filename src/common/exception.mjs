74b9051a4dd27524d235d68c63b03d1bef05f50b
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-05-25 01:29:14 +0100

    Exception

--- src/common/exception.mjs
+++ src/common/exception.mjs
@@ -10,36 +10,36 @@ const log = debug('zashiki/common/exception')
 
 log('`zashiki` is awake')
 
-const transformException = ({
-  code = 500,
-  name = 'Internal Server Error',
-  message
-} = {}) => ({
-  code,
-  name,
-  ...(
-    message ? { message } : {}
-  )
-})
-
-export default (error) => ({
-  status: Signals.FAILURE,
-  exception: transformException(error)
-})
-
 export const NOT_FOUND = 'https://tools.ietf.org/html/rfc7231#section-6.5.4'
 export const BAD_DATA = 'https://tools.ietf.org/html/rfc7231#section-6.5.1'
 export const BAD_IMPLEMENTATION = 'https://tools.ietf.org/html/rfc7231#section-6.5.1'
 
-export function handleException (error) {
-  const {
+export function transformException ({
+  code = 500,
+  name = 'Internal Server Error',
+  message
+} = {}) {
+  return {
     code,
-    message = 'No error message defined'
-  } = error
+    name,
+    ...(message ? { message } : {})
+  }
+}
 
+export function handleException ({
+  code,
+  message = 'No error message defined'
+} = {}) {
   if (code) log(code, message)
   else log(message)
 
   if (code === 404) throw Boom.notFound(NOT_FOUND)
   throw Boom.badImplementation(BAD_IMPLEMENTATION)
+}
+
+export default function exception (error) {
+  return {
+    status: Signals.FAILURE,
+    exception: transformException(error)
+  }
 }
